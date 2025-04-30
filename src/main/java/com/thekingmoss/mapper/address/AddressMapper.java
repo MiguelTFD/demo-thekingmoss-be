2a62ab224c7338badfe6c5cc271716753ca5792b
Diego <gimbletdev@gmail.com>
2025-04-30 13:59:34 -0500

    Feat: Added AddressMapper Class

---
+++ src/main/java/com/thekingmoss/mapper/address/AddressMapper.java
@@ -0,0 +1,44 @@
+package com.thekingmoss.mapper.address;
+
+import com.thekingmoss.dto.address.AddressRequestDTO;
+import com.thekingmoss.dto.address.AddressResponseDTO;
+import com.thekingmoss.entity.Address;
+import com.thekingmoss.entity.User;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AddressMapper {
+    public AddressResponseDTO toDto(Address address) {
+        return AddressResponseDTO.builder()
+                .addressId(address.getAddressId())
+                .country(address.getCountry())
+                .state(address.getState())
+                .province(address.getProvince())
+                .district(address.getDistrict())
+                .addressLine(address.getAddressLine())
+                .addressType(address.getAddressType())
+                .userId(address.getUser().getUserId())
+                .username(address.getUser().getUsername())
+                .password(address.getUser().getPassword())
+                .firstName(address.getUser().getFirstName())
+                .lastName(address.getUser().getLastName())
+                .phone(address.getUser().getPhone())
+                .email(address.getUser().getEmail())
+                .roleId(address.getUser().getRole().getRoleId())
+                .roleName(address.getUser().getRole().getName())
+                .build();
+    }
+
+    public Address toEntity(AddressRequestDTO requestDTO, User user) {
+        return Address.builder()
+                .country(requestDTO.getCountry())
+                .state(requestDTO.getState())
+                .province(requestDTO.getProvince())
+                .district(requestDTO.getDistrict())
+                .addressLine(requestDTO.getAddressLine())
+                .addressReference(requestDTO.getAddressReference())
+                .addressType(requestDTO.getAddressType())
+                .user(user)
+                .build();
+    }
+}
