a1ff5d0255377fd4d22aa194717f20e1dd21c7cd
Diego <gimbletdev@gmail.com>
2025-04-30 20:26:43 -0500

    fix : Add missing parameter from IAddressService Interface saveAddress Method

--- src/main/java/com/thekingmoss/service/IAddressService.java
+++ src/main/java/com/thekingmoss/service/IAddressService.java
@@ -2,6 +2,7 @@ package com.thekingmoss.service;
 
 import com.thekingmoss.dto.address.AddressRequestDTO;
 import com.thekingmoss.dto.address.AddressResponseDTO;
+import com.thekingmoss.entity.User;
 
 import java.util.List;
 
@@ -9,7 +10,7 @@ public interface IAddressService {
     List<AddressResponseDTO> listAddresses();
     List<AddressResponseDTO> listAddressesByUserId(Long id);
     AddressResponseDTO getAddressById(Long id);
-    AddressResponseDTO saveAddress(AddressRequestDTO requestDTO);
-    AddressResponseDTO updateAddressById(AddressRequestDTO requestDTO);
+    AddressResponseDTO saveAddress(AddressRequestDTO requestDTO, User user);
+    AddressResponseDTO updateAddressByUserId(AddressRequestDTO requestDTO);
     void deleteAddressById(Long id);
 }
